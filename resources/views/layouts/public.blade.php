66f28b1290ad98209b1f37995fb680b15d1f65d0
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-17 22:23:31 +0330

    consolidate public page shell

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -19,31 +19,71 @@
 
     <meta
         name="theme-color"
-        content="#0b0c0d"
+        content="@hasSection('discover_page') #0d0f10 @else #0b0c0d @endif"
     >
 
-    <title>@yield('title', 'NOBAT')</title>
+    <title>
+        @yield('title', 'NOBAT')
+    </title>
+
+    @hasSection('description')
+        <meta
+            name="description"
+            content="@yield('description')"
+        >
+    @else
+        <meta
+            name="description"
+            content="@yield('meta_description', 'NOBAT؛ پیدا کن، مقایسه کن و نوبت بگیر.')"
+        >
+    @endif
 
     <meta
-        name="description"
-        content="@yield('meta_description', 'NOBAT؛ پیدا کن، مقایسه کن و نوبت بگیر.')"
+        name="robots"
+        content="@yield('robots', 'index,follow')"
     >
 
-    @vite([
-        'resources/css/app.css',
-        'resources/css/public-salon.css',
-        'resources/js/app.js',
-        'resources/js/customer.js',
-    ])
+    <link
+        rel="canonical"
+        href="@yield('canonical', url()->current())"
+    >
+
+    @hasSection('discover_page')
+        @vite([
+            'resources/css/app.css',
+            'resources/css/discovery.css',
+            'resources/js/app.js',
+            'resources/js/discover.js',
+        ])
+    @else
+        @vite([
+            'resources/css/app.css',
+            'resources/css/public-salon.css',
+            'resources/js/app.js',
+            'resources/js/customer.js',
+        ])
+    @endif
 
     @stack('styles')
     @stack('head')
 </head>
 
 <body class="min-h-screen bg-black text-white antialiased">
-    <main>
-        @yield('content')
-    </main>
+    @hasSection('discover_page')
+        <div class="discover-shell">
+            @include('public.navigation')
+
+            <main>
+                @yield('content')
+            </main>
+
+            @include('public.mobile-navigation')
+        </div>
+    @else
+        <main>
+            @yield('content')
+        </main>
+    @endif
 
     @stack('scripts')
 </body>
