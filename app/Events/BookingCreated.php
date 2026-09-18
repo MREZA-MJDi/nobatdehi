a563c6a09cb719ef1f3cd2f4e465194ba783ccd3
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-18 20:59:31 +0330

    add booking notification core BookingCreated.php

---
+++ app/Events/BookingCreated.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Booking;
+use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class BookingCreated implements ShouldDispatchAfterCommit
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct(
+        public Booking $booking
+    ) {
+    }
+}
