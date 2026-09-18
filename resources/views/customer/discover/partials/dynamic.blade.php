ea2b4de6eb2e6017b3468444c94908bb1232681a
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-19 02:26:05 +0330

    Add lightweight Discover AJAX partial

---
+++ resources/views/customer/discover/partials/dynamic.blade.php
@@ -0,0 +1,25 @@
+@php
+    $resolveImage = function ($path) {
+        if (! $path) {
+            return null;
+        }
+
+        if (
+            str_starts_with($path, 'http://') ||
+            str_starts_with($path, 'https://') ||
+            str_starts_with($path, '/')
+        ) {
+            return $path;
+        }
+
+        return \Illuminate\Support\Facades\Storage::url($path);
+    };
+@endphp
+
+<div id="discoverDynamicContent">
+    @if($hasGeo)
+        @include('customer.discover.sections.nearby')
+    @endif
+
+    @include('customer.discover.sections.results')
+</div>
