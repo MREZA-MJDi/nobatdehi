0a5bf67d7aacc6ebdaaaa991eeb4a6d013f96aeb
MREZA-MJDi <rm2025studio@gmail.com>
2026-09-25 04:23:24 +0330

    Register Discover JS in Vite build

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
 
                 'resources/css/customer.css',
                 'resources/js/customer.js',
+                'resources/js/discover.js',
 
                 'resources/css/discovery.css',
                 'resources/css/salon.css',
